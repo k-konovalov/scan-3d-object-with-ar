88c686e31c8823bcb867fccdeb032e76b18c7e2c
lastic21 <lastic789@yandex.ru>
2020-09-02 11:28:59 +0300

    implemented scenarioScreen xml and created SingleViewModel.kt with cameraX

--- app/src/main/java/com/arvrlab/reconstructcamera/MainActivity.kt
+++ app/src/main/java/com/arvrlab/reconstructcamera/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.arvrlab.reconstructcamera
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.view.PreviewView
+import androidx.core.view.doOnLayout
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
+    private val singleViewModel: SingleViewModel by viewModels()
+    private val permissions = arrayOf("android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestPermissions(permissions,0)
 
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         viewPager.apply {
             adapter = ViewPagerAdapterAdapter(supportFragmentManager)
             currentItem = 0
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissions.forEachIndexed { index, s ->
+            if (s == permissions[index] && grantResults[index] == PackageManager.PERMISSION_GRANTED)
+                pvPreview.doOnLayout { singleViewModel.cameraX.initCamera(this, it as PreviewView) }
         }
     }
 }
--- app/src/main/java/com/arvrlab/reconstructcamera/ui/main/MainFragment.kt
+++ app/src/main/java/com/arvrlab/reconstructcamera/ui/main/MainFragment.kt
@@ -1,26 +1,26 @@
 package com.arvrlab.reconstructcamera.ui.main
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.camera.view.PreviewView
 import androidx.core.view.doOnLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.arvrlab.reconstructcamera.CustomCameraX
 import com.arvrlab.reconstructcamera.R
+import com.arvrlab.reconstructcamera.SingleViewModel
 import com.warkiz.widget.IndicatorSeekBar
 import com.warkiz.widget.OnSeekChangeListener
 import com.warkiz.widget.SeekParams
+import kotlinx.android.synthetic.main.main_activity.*
 import kotlinx.android.synthetic.main.main_fragment.*
 import java.io.File
 
 class MainFragment : Fragment(R.layout.main_fragment) {
     val viewModel: MainViewModel by viewModels()
-    val cameraX = CustomCameraX()
-    val permissions = arrayOf("android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE")
+    val singleViewModel : SingleViewModel by activityViewModels()
 
     private val outputDirectory: File by lazy { getOutputDir() }
 
@@ -34,9 +34,8 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requestPermissions(permissions,0)
 
-        cameraX.logAndSetupAvailableCameraSettings(requireContext())
+        singleViewModel.cameraX.logAndSetupAvailableCameraSettings(requireContext())
 
         initCameraXObservers()
         initSeekbarsListeners()
@@ -48,7 +47,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     private fun initSeekbarsListeners(){
         sbWb.onSeekChangeListener = object : OnSeekChangeListener{
             override fun onSeeking(seekParams: SeekParams?) {
-                cameraX.wb.postValue(seekParams?.progress)
+                singleViewModel.cameraX.wb.postValue(seekParams?.progress)
             }
 
             override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {}
@@ -56,7 +55,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         }
         sbFocus.onSeekChangeListener = object : OnSeekChangeListener{
             override fun onSeeking(seekParams: SeekParams?) {
-                cameraX.focus.postValue(seekParams?.progress)
+                singleViewModel.cameraX.focus.postValue(seekParams?.progress)
             }
 
             override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {}
@@ -64,7 +63,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         }
         sbISO.onSeekChangeListener = object : OnSeekChangeListener{
             override fun onSeeking(seekParams: SeekParams?) {
-                cameraX.iso.postValue(seekParams?.progress?.plus(50)) // Min ISO always 50
+                singleViewModel.cameraX.iso.postValue(seekParams?.progress?.plus(50)) // Min ISO always 50
             }
 
             override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {}
@@ -72,7 +71,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         }
         sbShutter.onSeekChangeListener = object : OnSeekChangeListener{
             override fun onSeeking(seekParams: SeekParams?) {
-                cameraX.shutter.postValue(seekParams?.progress)
+                singleViewModel.cameraX.shutter.postValue(seekParams?.progress)
             }
 
             override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {}
@@ -90,40 +89,40 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     private fun initSwitchListeners() {
         sAF.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) cameraX.autoFocus.postValue(true)
-            else cameraX.autoFocus.postValue(false)
+            if(isChecked) singleViewModel.cameraX.autoFocus.postValue(true)
+            else singleViewModel.cameraX.autoFocus.postValue(false)
         }
         sAutoIsoShutter.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) cameraX.autoExposition.postValue(true)
-            else cameraX.autoExposition.postValue(false)
+            if(isChecked) singleViewModel.cameraX.autoExposition.postValue(true)
+            else singleViewModel.cameraX.autoExposition.postValue(false)
         }
         sAutoWB.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) cameraX.autoWB.postValue(true)
-            else cameraX.autoWB.postValue(false)
+            if(isChecked) singleViewModel.cameraX.autoWB.postValue(true)
+            else singleViewModel.cameraX.autoWB.postValue(false)
         }
         sFlash.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) cameraX.flash.postValue(true)
-            else cameraX.flash.postValue(false)
+            if(isChecked) singleViewModel.cameraX.flash.postValue(true)
+            else singleViewModel.cameraX.flash.postValue(false)
         }
     }
 
     private fun initOnClickListeners() {
         fabTakePicture.setOnClickListener {
-            cameraX.takePhoto(outputDirectory, requireContext())
+            singleViewModel.cameraX.takePhoto(outputDirectory, requireContext())
         }
         btnStartTimer.setOnClickListener {
             if (etDelayBetweenPhoto.text.isNotEmpty() && etNumberOfPhotos.text.isNotEmpty())
-                cameraX.initPhotoTimer(requireContext(),etDelayBetweenPhoto.text.toString().toLong(), etNumberOfPhotos.text.toString().toLong() ,outputDirectory)
+                singleViewModel.cameraX.initPhotoTimer(requireContext(),etDelayBetweenPhoto.text.toString().toLong(), etNumberOfPhotos.text.toString().toLong() ,outputDirectory)
             else Toast.makeText(requireContext(),"Timer settings is empty?", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun initCameraXObservers(){
         val observerForCameraChange = Observer<Any> { _ ->
-            pvPreview.doOnLayout { cameraX.initCamera(viewLifecycleOwner, it as PreviewView) }
+            requireActivity().pvPreview.doOnLayout { singleViewModel.cameraX.initCamera(viewLifecycleOwner, it as PreviewView) }
         }
 
-        cameraX.run {
+        singleViewModel.cameraX.run {
             logAndSetupAvailableCameraSettings(requireContext())
 
             wb.observe(viewLifecycleOwner, observerForCameraChange)
@@ -148,13 +147,5 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     private fun showToastWith(it: String?) {
         Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        permissions.forEachIndexed { index, s ->
-            if (s == permissions[index] && grantResults[index] == PackageManager.PERMISSION_GRANTED)
-                pvPreview.doOnLayout { cameraX.initCamera(viewLifecycleOwner, it as PreviewView) }
-        }
     }
 }
