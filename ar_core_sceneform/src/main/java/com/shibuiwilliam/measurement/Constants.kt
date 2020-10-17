0cdf51d4b703b1873fe8902c80549c5e2eaad7c0
konovalovkirill <konovalov.k.a@yandex.ru>
2020-10-17 17:20:50 +0300

    Refactor package name

--- ar_core_sceneform/src/main/java/com/shibuiwilliam/measurement/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.shibuiwilliam.measurement
-
-object Constants{
-    const val maxNumMultiplePoints = 6
-    const val multipleDistanceTableHeight = 300
-
-    const val arrowViewSize = 45
-}
--- ar_core_sceneform/src/main/java/com/shibuiwilliam/measurement/Triangle.kt
+++
@@ -1,7 +0,0 @@
-package com.shibuiwilliam.measurement
-
-data class Triangle(
-    var currentObjectVector: Float,
-    var previousCameraVector: Float,
-    var currentCameraVector: Float
-)
